274dc83bf9957a8eeb1dc0e5bea4e8e1b9412e3e
Steffan Andrews <stef@orchetect.com>
2026-07-12 14:21:04 -0700

    `Sequence<PID>`: Added `contains(whereFileDescriptorVNodePath:)`, `filter(whereFileDescriptorVNodePath:)` and `fileDescriptorVNodePaths(whereVNodePath:)` methods

--- Sources/SwiftProcess/PID/Sequence+PID.swift
+++ Sources/SwiftProcess/PID/Sequence+PID.swift
@@ -17,4 +17,35 @@ extension Sequence<PID> {
     public func contains(pid rawPID: PID.RawValue) -> Bool {
         contains { $0.rawValue == rawPID }
     }
+
+    /// Returns a Boolean value indicating whether the sequence contains PIDs that have VNode file descriptors
+    /// matching the given path predicate.
+    nonisolated
+    public func contains(whereFileDescriptorVNodePath predicate: @escaping (_ path: String) -> Bool) -> LazyFilterSequence<Self> {
+        lazy.filter {
+            $0.fileDescriptors.contains(whereVNodePath: predicate)
+        }
+    }
+
+    /// Returns a lazy filter sequence of PIDs that have VNode file descriptors matching the given path predicate.
+    nonisolated
+    public func filter(whereFileDescriptorVNodePath predicate: @escaping (_ path: String) -> Bool) -> LazyFilterSequence<Self> {
+        lazy.filter {
+            $0.fileDescriptors.contains(whereVNodePath: predicate)
+        }
+    }
+
+    /// Filters the collection to only PIDs that have VNode file descriptors matching the given path predicate,
+    /// returning a dictionary keyed by PID with an array value of matching paths.
+    nonisolated
+    public func fileDescriptorVNodePaths(whereVNodePath predicate: @escaping (_ path: String) -> Bool) -> [PID: [String]] {
+        lazy.reduce(into: [:]) { dict, pid in
+            let paths: [String] = pid.fileDescriptors.compactMap {
+                guard case let .vNode(fd: _, path: path) = $0 else { return nil }
+                guard predicate(path) else { return nil }
+                return path
+            }
+            if !paths.isEmpty { dict[pid] = paths }
+        }
+    }
 }
