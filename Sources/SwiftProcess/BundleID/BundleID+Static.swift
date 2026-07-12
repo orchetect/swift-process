0f55d28c441a73144eae1486970c927a992ce043
Steffan Andrews <stef@orchetect.com>
2026-07-11 19:33:43 -0700

    `BundleID`: `main` static property symbol is now available on all platforms

--- Sources/SwiftProcess/BundleID/BundleID+Static.swift
+++ Sources/SwiftProcess/BundleID/BundleID+Static.swift
@@ -4,18 +4,20 @@
 //  © 2026 Steffan Andrews • Licensed under MIT License
 //
 
-// Bundle ID lookup is only available on macOS (not including macCatalyst)
-
-#if os(macOS)
-
 import class Foundation.Bundle
 
 extension BundleID {
     /// Returns the bundle identifier for the current process's main bundle.
+    ///
+    /// > Note: Bundle ID lookup is only available on macOS (not including Mac Catalyst).
+    /// > On all other platforms, this property always returns `nil`.
     public static var main: BundleID? {
+        #if os(macOS)
         guard let string = Bundle.main.bundleIdentifier else { return nil }
         return BundleID(string)
+        #else
+        return nil
+        #endif
     }
 }
 
-#endif
