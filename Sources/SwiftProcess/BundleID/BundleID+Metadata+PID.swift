646d7257cb260aeb76ad560c1c5001b1caaad122
Steffan Andrews <stef@orchetect.com>
2026-07-11 19:24:12 -0700

    `BundleID`: `pids` property symbol is now available on all platforms

--- Sources/SwiftProcess/BundleID/BundleID+Metadata+PID.swift
+++ Sources/SwiftProcess/BundleID/BundleID+Metadata+PID.swift
@@ -7,13 +7,23 @@
 // Bundle ID lookup is only available on macOS (not including macCatalyst)
 
 #if os(macOS)
-
 import class AppKit.NSRunningApplication
+#endif
 
+@available(macOS 10.15, *)
+@available(macCatalyst, deprecated, message: "Not available on macCatalyst.")
+@available(iOS, deprecated, message: "Not available on iOS.")
+@available(tvOS, deprecated, message: "Not available on tvOS.")
+@available(watchOS, deprecated, message: "Not available on watchOS.")
+@available(visionOS, deprecated, message: "Not available on visionOS.")
 extension BundleID {
     /// Returns all process identifiers for running processes associated with the specified bundle identifier.
+    ///
+    /// > Note: Bundle ID lookup is only available on macOS (not including Mac Catalyst).
+    /// > On all other platforms, this property always returns an empty collection.
     nonisolated
     public var pids: Set<PID> {
+        #if os(macOS)
         let runningApps = NSRunningApplication
             .runningApplications(withBundleIdentifier: rawValue)
 
@@ -22,7 +32,8 @@ extension BundleID {
             .map(PID.init(rawValue:))
 
         return Set(pids)
+        #else
+        return []
+        #endif
     }
 }
-
-#endif
