671659c06b5a8588523c33b75806ddc52e5c602f
Steffan Andrews <stef@orchetect.com>
2026-07-12 23:20:45 -0700

    `Sequence<PID>`: Removed `lsofPaths(whereName:)` as it is long-running and not viable

--- Sources/SwiftProcess/PID/Sequence+PID.swift
+++ Sources/SwiftProcess/PID/Sequence+PID.swift
@@ -48,40 +48,4 @@ extension Sequence<PID> {
             if !paths.isEmpty { dict[pid] = paths }
         }
     }
-
-    /// Filters the collection to only PIDs that have file/port descriptor names as returned by the `lsof` command
-    /// matching the given path predicate, returning a dictionary keyed by PID with an array value of matching paths.
-    /// This typically contains a more thorough list of descriptors than calling ``fileDescriptors`` or ``filePorts``.
-    /// 
-    /// > Note: File descriptor lookup is only available on macOS (not including Mac Catalyst).
-    /// > On all other platforms, this property always throws an error.
-    @available(macOS 10.15.4, iOS 13.4, watchOS 6.2, tvOS 13.4, *)
-    nonisolated
-    public func lsofPaths(whereName predicate: @escaping (_ name: String) -> Bool) throws(PID.SystemError) -> [PID: [String]] {
-        var dict: [PID: [String]] = [:]
-        for pid in self {
-            let paths: [String] = try pid.lsofDescriptors()
-                .filter { predicate($0) }
-            if !paths.isEmpty { dict[pid] = paths }
-        }
-        return dict
-    }
-
-    /// Filters the collection to only PIDs that have file/port descriptor names as returned by the `lsof` command
-    /// matching the given path predicate, returning a dictionary keyed by PID with an array value of matching paths.
-    /// This typically contains a more thorough list of descriptors than calling ``fileDescriptors`` or ``filePorts``.
-    ///
-    /// > Note: File descriptor lookup is only available on macOS (not including Mac Catalyst).
-    /// > On all other platforms, this property always throws an error.
-    @available(macOS 10.15.4, iOS 13.4, watchOS 6.2, tvOS 13.4, *)
-    @concurrent
-    public func lsofPaths(whereName predicate: @escaping (_ name: String) -> Bool) async throws(PID.SystemError) -> [PID: [String]] {
-        var dict: [PID: [String]] = [:]
-        for pid in self {
-            let paths: [String] = try await pid.lsofDescriptors()
-                .filter { predicate($0) }
-            if !paths.isEmpty { dict[pid] = paths }
-        }
-        return dict
-    }
 }
